f61cb049283175e1320ceed55e6eba92dce84ff9
q-apps <21a.kovaleva@gmail.com>
2018-12-17 21:44:52 +0300

    First Nim Game

---
+++ src/Nim.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Nim extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      barsCount: this.props.count,
+      currentPlayer : 1
+    }
+  }
+
+  removeBars (amount) {
+    this.setState({
+      barsCount: this.state.barsCount - amount,
+      currentPlayer: 3 - this.state.currentPlayer
+    })
+  }
+
+  render () {
+    var barsView = " "
+    for (var i = 0; i < this.state.barsCount; i++ ) {
+      barsView +="|"
+
+    }
+    return (
+      <div>
+        <div id="stats">
+          <p><h3>Палочек на столе: {this.state.barsCount}</h3></p>
+          <p><h4>Ходит игрок {this.state.currentPlayer}</h4></p>
+        </div>
+        <div id="bars">
+        <h3>{barsView}</h3>
+        </div>
+        <div id="buttons">
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored" onClick={() => {this.removeBars(1)}}>1</button>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored" onClick={() => {this.removeBars(2)}}>2</button>
+          <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored" onClick={() => {this.removeBars(3)}}>3</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Nim;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Nim from './Nim';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<div>
+  <Nim count="100" />
+</div>
+
+
+
+  , document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
